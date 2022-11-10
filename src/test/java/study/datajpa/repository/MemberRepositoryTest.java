015b5f57d67308e877c29edeb80f988bf7f629fa
jjkim <jangju_kim@scllab.co.kr>
2022-11-10 14:21:39 +0900

    Query annotation 사용 (DTO 조회)

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
+import study.datajpa.entity.Team;
 
 import java.util.List;
 import java.util.Optional;
@@ -20,6 +22,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class MemberRepositoryTest {
 
     @Autowired MemberRepository memberRepository;
+    @Autowired TeamRepository teamRepository;
 
     @Test
     public void testMember() {
@@ -115,5 +118,36 @@ class MemberRepositoryTest {
         assertThat(result.get(0).getUsername()).isEqualTo("AAA");
         assertThat(result.get(0).getAge()).isEqualTo(20);
         assertThat(result.size()).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("findUsernameList")
+    void findUsernameList() throws Exception {
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("AAA", 20);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<String> result = memberRepository.findUsernameList();
+        for (String s : result) {
+            System.out.println("s = " + s);
+        }
+
+    }
+
+    @Test
+    @DisplayName("findMemberDto")
+    void findMemberDto() throws Exception {
+        Team team = new Team("teamA");
+        teamRepository.save(team);
+        
+        Member m1 = new Member("AAA", 10);
+        memberRepository.save(m1);
+
+        List<MemberDto> memberDto  = memberRepository.findMemberDto();
+        for (MemberDto dto : memberDto) {
+            System.out.println("memberDto = " + memberDto);
+        }
+        
     }
 }
